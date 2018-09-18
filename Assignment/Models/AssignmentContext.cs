29f457762cf9929b893ef30e081ab8bbe67df24c
Callum <cbuchanan.buchanan@gmail.com>
2018-09-18 15:44:18 +1200

    First commit

---
+++ Assignment/Models/AssignmentContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Assignment.Models
+{
+    public partial class AssignmentContext : DbContext
+    {
+        public virtual DbSet<Courses> Courses { get; set; }
+
+        public AssignmentContext(DbContextOptions<AssignmentContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Courses>(entity =>
+            {
+                entity.HasKey(e => e.Code);
+
+                entity.Property(e => e.Code)
+                    .HasColumnName("code")
+                    .HasColumnType("char(4)")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Compulsory)
+                    .IsRequired()
+                    .HasColumnName("compulsory")
+                    .HasColumnType("char(1)");
+
+                entity.Property(e => e.Description).HasColumnName("description");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.PrereqCourseCode)
+                    .HasColumnName("prereq_course_code")
+                    .HasColumnType("char(4)");
+
+                entity.Property(e => e.SemesterOffered).HasColumnName("semester_offered");
+
+                entity.Property(e => e.SemesterOffered2).HasColumnName("semester_offered_2");
+            });
+        }
+    }
+}
---
+++ Assignment/Models/Courses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment.Models
+{
+    public partial class Courses
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int SemesterOffered { get; set; }
+        public int? SemesterOffered2 { get; set; }
+        public string PrereqCourseCode { get; set; }
+        public string Compulsory { get; set; }
+    }
+}
